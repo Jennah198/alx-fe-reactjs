125812b6c7ed4ff36da65928a630238ca5cfc617
Nezira <neziraworku198@gmail.com>
2025-03-09 10:54:41 -0700

    Enhanced Recipe Sharing App with editing, deleting, and detailed views

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
-import { useRecipeStore } from './components/recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from "./recipeStore";
 
 
 const RecipeList = () => {
@@ -10,6 +11,7 @@ const RecipeList = () => {
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
