ab9eda844a46a240adb97a1911ecb58cd5833459
Nezira <neziraworku198@gmail.com>
2025-03-09 17:47:46 -0700

    feat: integrate Formik with Yup validation for form handling

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Form Handling with Formik</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
