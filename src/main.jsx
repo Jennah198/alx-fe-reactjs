7a0c41fdba4f0e45995277412b2af797c3c3a9fc
Nezira <neziraworku198@gmail.com>
2025-03-26 03:37:51 -0700

    fix: Resolve merge conflicts and update package.json, package-lock.json, index.css, and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Adjust path if necessary
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -20,4 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
->>>>>>> 9173e27 (feat: Implement user registration form with controlled components and Formik)
